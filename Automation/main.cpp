4cf0596afc39b310f5554538b6aa3c0f6e1ad927
Adamantan <adamantanvgmail.com>
2014-10-07 21:45:28 +0200

    Version 0.0001

--- Automation/main.cpp
+++ Automation/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <fstream>
 
 using namespace std;
 
+
+int graph[6][6] = {{0, 10, 0, 0, 0, 0},
+                   {10, 0, 10, 30, 0, 0},
+                   {0, 10, 0, 20, 50, 0},
+                   {0, 30, 20, 0, 0, 10},
+                   {0, 0, 50, 0, 0, 10},
+                   {0, 0, 0, 10, 10, 0}};
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    system("pause");
     return 0;
 }
