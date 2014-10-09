712b4e0928f19fca2e89bbe630a2505467a2f7ca
Nemanja <nemanja.filipovic96@gmail.com>
2014-10-09 21:58:00 +0200

    Added arrays of gBins and gTrucks and also defined structure types.

--- Automation/main.cpp
+++ Automation/main.cpp
@@ -3,15 +3,31 @@
 #include <stdio.h>
 #include <fstream>
 
-
-#define _LENGTH 6
+//Constants for additional change
+#define _LENGTH  6
 #define _inf 2147483646
+#define maxVolume 1000
+#define maxFuel 150
+#define gTruckCnt 1
+
+
+//----------
 
 using namespace std;
 
 int origin = 0;
+typedef struct{
+    int gVolumeRemaining;
+    int tDistance;
+    int Fuel;
+} gTruck;
 
-
+typedef struct{
+    int gProduction[7];
+    int tID;
+    int gCurrent;
+    int pickUpID;
+} gBin;
 int graph[6][6] = {{0, 10, 0, 0, 0, 0},
                    {10, 0, 10, 30, 0, 0},
                    {0, 10, 0, 20, 50, 0},
@@ -20,7 +36,22 @@ int graph[6][6] = {{0, 10, 0, 0, 0, 0},
                    {0, 0, 0, 10, 10, 0}};
 
 int nodes[2][_LENGTH] = {{_inf,_inf,_inf,_inf,_inf,_inf}, {_inf,_inf,_inf,_inf,_inf,_inf}};
+gTruck gTruckArr[gTruckCnt];
+gBin gBinArr[_LENGTH];
 
+void InitVars(int numGTrucks, int nGBin){
+    for(int i = 0; i<gTruckCnt; i++){
+        gTruckArr[i].gVolumeRemaining = maxVolume;
+        gTruckArr[i].Fuel = maxFuel;
+        gTruckArr[i].tDistance = 0;
+    }
+    for(int i = 0; i<_LENGTH; i++){
+        gBinArr[i].gCurrent = 0;
+        gBinArr[i].tID = -1;
+        gBinArr[i].pickUpID = -1;
+        for(int y = 0; y<7; y++) gBinArr[i].gProduction[y] = 0;
+    }
+}
 
 void relax(int current_node){
     for(int i=0; i<_LENGTH; i++){
